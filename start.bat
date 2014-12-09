27cc72561ce4704c237373e73f3bf31a2aeb4d39
michael34435 <michael34435@gmail.com>
2014-12-09 21:25:45 +0800

    add env var

--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
+SET PATH=%PATH%;C:\xampp\php;C:\php;C:\Program Files (x86)\xampp\php
 START "" php -S 127.0.0.1:55555
 START "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:55555"
